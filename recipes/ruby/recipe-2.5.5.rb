da3ea1725e0282d1f1140da4ff010559f236d14a
Bruce Wang <number5@users.noreply.github.com>
2019-04-15 12:14:00 +1000

    fix nokogiri compile error by adding libgmp-dev as deps

--- recipes/ruby/recipe-2.5.5.rb
+++ recipes/ruby/recipe-2.5.5.rb
@@ -3,7 +3,7 @@ class Ruby255 < FPM::Cookery::Recipe
 
   name 'ruby'
   version '1:2.5.5'
-  revision 0
+  revision 1
   homepage 'http://www.ruby-lang.org/'
   source 'https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.5.tar.gz'
   sha256 '28a945fdf340e6ba04fc890b98648342e3cccfd6d223a48f3810572f11b2514c'
@@ -12,9 +12,10 @@ class Ruby255 < FPM::Cookery::Recipe
 
   build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
                 'libssl-dev', 'libyaml-dev'
-
+  
+  # we have to put libgmp-dev here for ruby able to compile ext like nokogiri, see https://github.com/docker-library/ruby/issues/267
   depends 'libffi6', 'libncurses5', 'libreadline6', 'libssl1.0.0',
-          'libtinfo5', 'libyaml-0-2', 'zlib1g'
+          'libtinfo5', 'libyaml-0-2', 'zlib1g', 'libgmp-dev'
 
   def build
     configure :prefix => prefix, 'disable-install-doc' => true
